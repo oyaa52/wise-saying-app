8985cf3faabc8aff81a97dbb3a7f935f7e9ab429
yeonjilim <imyeonji520@gmail.com>
2024-11-19 14:31:44 +0900

    Until Level 5

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,17 +1,55 @@
 package com.ll;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static int totalData = 0;
+    static ArrayList<String[]> list = new ArrayList<>();
+    public static class Data {
+        String[] data = new String[3];
+        String quotes;
+        String name;
+
+        public Data(String quotes, String name) {
+            this.quotes = quotes;
+            this.name = name;
+            totalData++;
         }
+
+        public String[] save(String quotes, String name) {
+            data[0] = String.valueOf(totalData);
+            data[1] = quotes;
+            data[2] = name;
+            list.add(data);
+            return data;
+        }
+
+        public void saveMessage() {
+            System.out.println(data[0] + "번 명언이 등록되었습니다.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text;
+        System.out.println("== 명언 앱 ==");
+        do {
+            System.out.print("명령) ");
+            text = scanner.nextLine();
+            if (text.equals("등록")) {
+                System.out.print("명언: ");
+                String quotes = scanner.nextLine();
+                System.out.print("작가: ");
+                String name = scanner.nextLine();
+                Data data = new Data(quotes, name);
+                data.save(quotes, name);
+                data.saveMessage();
+            } else if (text.equals("목록")) {
+                for (String[] value : list) {
+                    System.out.println(value[0] + " | " + value[2] + " | " + value[1]);
+                }
+            }
+        } while (!text.equals("종료"));
     }
 }
