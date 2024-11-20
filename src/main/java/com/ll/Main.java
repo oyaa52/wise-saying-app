f6b658f6791e434d45288ba3d6e56d3736249d3e
yeonjilim <imyeonji520@gmail.com>
2024-11-20 12:02:54 +0900

    Move main logic to App class

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,42 +2,53 @@ package com.ll;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Main {
-    static int totalData = 0;
-    static ArrayList<String[]> list = new ArrayList<>();
-
-    public static class Data {
-        String[] data = new String[3];
-        String quotes;
-        String name;
-
-        public Data(String quotes, String name) {
-            this.quotes = quotes;
-            this.name = name;
-            totalData++;
-        }
-
-        public String[] save(String quotes, String name) {
-            data[0] = String.valueOf(totalData);
-            data[1] = quotes;
-            data[2] = name;
-            list.add(data);
-            return data;
-        }
-
-        public void saveMessage() {
-            System.out.println(data[0] + "번 명언이 등록되었습니다.");
-        }
-    }
-
     public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+}
+
+class App {
+    int totalData = 0;
+    ArrayList<String[]> list = new ArrayList<>();
+
+    public void run() {
+        class Data {
+            String[] data = new String[3];
+            String quotes;
+            String name;
+
+            public Data(String quotes, String name) {
+                this.quotes = quotes;
+                this.name = name;
+                totalData++;
+            }
+
+            public String[] save(String quotes, String name) {
+                data[0] = String.valueOf(totalData);
+                data[1] = quotes;
+                data[2] = name;
+                list.add(data);
+                return data;
+            }
+
+            public void saveMessage() {
+                System.out.println(data[0] + "번 명언이 등록되었습니다.");
+            }
+        }
+
         Scanner scanner = new Scanner(System.in);
         String text;
         System.out.println("== 명언 앱 ==");
         do {
             System.out.print("명령) ");
             text = scanner.nextLine();
+            String index = String.valueOf(text.charAt(text.length() - 1));
             if (text.equals("등록")) {
                 System.out.print("명언: ");
                 String quotes = scanner.nextLine();
@@ -51,7 +62,6 @@ public class Main {
                     System.out.println(value[0] + " | " + value[2] + " | " + value[1]);
                 }
             } else if (text.contains("삭제?id=")) {
-                String index = String.valueOf(text.charAt(text.length() - 1));
                 boolean isFound = false;
                 for (String[] value : list) {
                     if (index.equals(value[0])) {
@@ -65,7 +75,6 @@ public class Main {
                     System.out.println(index + "번 명언은 존재하지 않습니다.");
                 }
             } else if (text.contains("수정?id=")) {
-                String index = String.valueOf(text.charAt(text.length() - 1));
                 boolean isFound = false;
                 for (String[] value : list) {
                     if (index.equals(value[0])) {
