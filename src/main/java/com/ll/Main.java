7c1afac818dd17206b4d57915b4d2d865db067e5
yeonjilim <imyeonji520@gmail.com>
2024-11-22 16:26:51 +0900

    Refactoring

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -16,34 +16,17 @@ public class Main {
 
 class App {
     int totalData = 0;
-    ArrayList<String[]> list = new ArrayList<>();
+    List<WiseSaying> list = new ArrayList<>();
     File directory = new File("db/wiseSaying");
 
+    public void save(WiseSaying wiseSaying) {
+        list.add(wiseSaying);
+        System.out.println(wiseSaying.id + "번 명언이 등록되었습니다.");
+    }
+
     public void run() {
-        class Data {
-            String[] data = new String[3];
-            String quotes;
-            String name;
-
-            public Data(String quotes, String name) {
-                this.quotes = quotes;
-                this.name = name;
-                totalData++;
-            }
-
-            public void save(String quotes, String name) {
-                data[0] = String.valueOf(totalData);
-                data[1] = quotes;
-                data[2] = name;
-                list.add(data);
-//                list.add(new String[]{String.valueOf(index), quotes, name});
-            }
-
-            public void saveMessage() {
-                System.out.println(data[0] + "번 명언이 등록되었습니다.");
-            }
-        }
         checkHistory();
+
         Scanner scanner = new Scanner(System.in);
         String text;
         System.out.println("== 명언 앱 ==");
@@ -57,23 +40,22 @@ class App {
                 String quotes = scanner.nextLine();
                 System.out.print("작가: ");
                 String name = scanner.nextLine();
-                Data data = new Data(quotes, name);
-                data.save(quotes, name);
-                data.saveMessage();
-                fileUtil(data.data, "json");
+                WiseSaying wiseSaying = new WiseSaying(++totalData, quotes, name);
+                save(wiseSaying);
+                FileUtil.updateWiseSayingFile(wiseSaying);
             } else if (text.equals("목록")) {
                 System.out.println("번호 | 작가 | 명언");
                 System.out.println("--------------------");
-                for (String[] value : list) {
-                    System.out.println(value[0] + " | " + value[2] + " | " + value[1]);
+                for (WiseSaying value : list.reversed()) {
+                    System.out.println(value.id + " | " + value.quotes + " | " + value.name);
                 }
             } else if (text.contains("삭제?id=")) {
                 boolean isFound = false;
-                for (String[] value : list) {
-                    if (index.equals(value[0])) {
+                for (WiseSaying value : list) {
+                    if (index.equals(String.valueOf(value.id))) {
                         list.remove(Integer.parseInt(index) - 1);
                         System.out.println(index + "번 명령이 삭제되었습니다.");
-                        deleteFile(index);
+                        FileUtil.deleteFile(index);
                         isFound = true;
                         break;
                     }
@@ -83,21 +65,20 @@ class App {
                 }
             } else if (text.contains("수정?id=")) {
                 boolean isFound = false;
-                for (String[] value : list) {
-                    if (index.equals(value[0])) {
-                        System.out.println("명언(기존): " + value[1]);
-                        System.out.println("명언(수정): ");
+                for (WiseSaying value : list) {
+                    if (index.equals(String.valueOf(value.id))) {
+                        System.out.println("명언(기존): " + value.quotes + "\n명언(수정): ");
                         String newQuotes = scanner.nextLine();
-                        System.out.println("작가(기존): " + value[2]);
-                        System.out.println("작가(수정): ");
+                        System.out.println("작가(기존): " + value.name + "\n작가(수정): ");
                         String newName = scanner.nextLine();
-                        if (!newQuotes.isEmpty() && !newQuotes.equals(value[1])) {
-                            value[1] = newQuotes;
+                        if (!newQuotes.isEmpty() && !newQuotes.equals(value.quotes)) {
+                            value.quotes = newQuotes;
                         }
-                        if (!newName.isEmpty() && !newName.equals(value[2])) {
-                            value[2] = newName;
+                        if (!newName.isEmpty() && !newName.equals(value.name)) {
+                            value.name = newName;
                         }
-                        fileUtil(value, "json");
+                        System.out.println(value.id + "번 명언이 수정되었습니다.");
+                        FileUtil.updateWiseSayingFile(value);
                         isFound = true;
                         break;
                     }
@@ -107,126 +88,103 @@ class App {
                 }
             } else if (text.equals("종료")) {
                 if (!list.isEmpty()) {
-                    String[] lastData = list.get(list.size() - 1);
-                    fileUtil(lastData, "txt");
+                    FileUtil.updateLastId(totalData);
                 } else {
                     scanner.close();
                 }
             } else if (text.equals("빌드")) {
-                StringBuilder combinedJson = new StringBuilder();
-                combinedJson.append("[\n");
-                File[] files = directory.listFiles();
-                if (files != null && files.length > 0) {
-                    for (int i = 0; i < files.length; i++) {
-                        File file = files[i];
-                        if (file.isFile() && !file.getName().equals("lastId.txt") && !file.getName().equals("data.json")) {
-                            try {
-                                String content = new String(Files.readAllBytes(Paths.get(file.getPath())));
-                                combinedJson.append(content.trim());
-                                if (i < files.length - 1) {
-                                    combinedJson.append(",\n");
-                                }
-                                combinedJson.append("\n");
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    combinedJson.append("]");
-                    try (FileWriter fileWriter = new FileWriter("db/wiseSaying/data.json")) {
-                        fileWriter.write(combinedJson.toString());
-                        System.out.println("data.json 파일의 내용이 갱신되었습니다.");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+                FileUtil.buildDataJsonFile();
             }
         }
         while (!text.equals("종료"));
     }
 
-    String fileNameFormat(String[] data, String fileType) {
-        if (fileType.equals("json")) {
-            return "db/wiseSaying/" + data[0] + ".json";
-        } else if (fileType.equals("txt")) {
-            return "db/wiseSaying/lastId.txt";
-        } else return "";
-    }
-
-    String inputFormat(String[] data, String fileType) {
-        if (fileType.equals("json")) {
-            return "{\n\"id\": " + data[0] + ",\n" +
-                    "\"content\": \"" + data[1] + "\",\n" +
-                    "\"author\": \"" + data[2] + "\"\n}";
-        } else if (fileType.equals("txt")) {
-            return String.valueOf(totalData);
-        } else return "";
-    }
-
     void checkHistory() {
-        String filePath = "db/wiseSaying/lastId.txt";
-        File fileLastId = new File(filePath);
-        if (!fileLastId.exists()) return;
-
-        try {
-            FileReader fileReader = new FileReader(fileLastId);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            try {
-                int lastId = Integer.parseInt(bufferedReader.readLine().trim());
-                if (lastId > 0) {
-                    System.out.println("프로그램 다시 시작...");
-                    System.out.println("마지막 id: " + lastId);
-//                    File directory = new File("db/wiseSaying");
-                    if (directory.exists() && directory.isDirectory()) {
-                        File[] files = directory.listFiles();
-                        if (files != null && files.length > 0) {
-                            for (File file : files) {
-                                if (file.isFile() && !file.getName().equals("lastId.txt") && !file.getName().equals("data.json")) {
-                                    try {
-                                        // TODO 이 부분은 ChatGPT 사용하고 이해 완전히 못했으므로 복습할 것
-                                        String[] lines = Files.readAllLines(Paths.get(file.getPath())).toArray(new String[0]);
-
-                                        String id = lines[1].split(":")[1].trim().replace(",", "").replace("\"", "");
-                                        String content = lines[2].split(":")[1].trim().replace(",", "").replace("\"", "");
-                                        String author = lines[3].split(":")[1].trim().replace(",", "").replace("\"", "");
-
-                                        list.add(new String[]{id, content, author});
-                                        totalData = lastId;
-                                    } catch (IOException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        int lastId = FileUtil.loadLastId();
+        if (lastId > 0) {
+            System.out.println("프로그램 다시 시작...");
+            List<WiseSaying> loadedWiseSayings = FileUtil.loadWiseSayings(lastId);
+            list.addAll(loadedWiseSayings);
+            totalData = lastId;
         }
-
     }
+}
 
-    void fileUtil(String[] data, String fileType) {
-        String fileName = fileNameFormat(data, fileType);
-        try {
-            File file = new File(fileName);
-            file.createNewFile();
-            FileWriter fw = new FileWriter(file);
-            BufferedWriter writer = new BufferedWriter(fw);
-            String input = inputFormat(data, fileType);
-            writer.write(input);
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+class WiseSaying {
+    int id;
+    String quotes;
+    String name;
+
+    public WiseSaying(int id, String quotes, String author) {
+        this.id = id;
+        this.quotes = quotes;
+        this.name = author;
+    }
+}
+
+class FileUtil {
+    private static final String BASE_DIRECTORY = "db/wiseSaying";
+
+    public static void createDirectory() {
+        File directory = new File(BASE_DIRECTORY);
+        if (!directory.exists()) {
+            directory.mkdirs();
         }
     }
 
-    void deleteFile(String index) {
-        File directory = new File("db/wiseSaying");
+    public static String createFileName(WiseSaying wiseSaying) {
+        return BASE_DIRECTORY + "/" + wiseSaying.id + ".json";
+    }
+
+    public static String wiseSayingToJson(WiseSaying wiseSaying) {
+        return "{\n\t\"id\": " + wiseSaying.id + ",\n" +
+                "\t\"content\": \"" + wiseSaying.quotes + "\",\n" +
+                "\t\"author\": \"" + wiseSaying.name + "\"\n}";
+    }
+
+    public static void writeToFile(String filePath, String content) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(content);
+        }
+    }
+
+    public static void readFromFile(String filePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            reader.readLine();
+        }
+    }
+
+    public static String loadHistory() {
+        String filePath = BASE_DIRECTORY + "/lastId.txt";
+        try {
+            return Files.readString(Paths.get(filePath)).trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "0";
+        }
+    }
+
+    public static void updateWiseSayingFile(WiseSaying wiseSaying) {
+        String fileName = createFileName(wiseSaying);
+        String content = wiseSayingToJson(wiseSaying);
+        try {
+            writeToFile(fileName, content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateLastId(int totalData) {
+        String filePath = BASE_DIRECTORY + "/lastId.txt";
+        try {
+            writeToFile(filePath, String.valueOf(totalData));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteFile(String index) {
+        File directory = new File(BASE_DIRECTORY);
         File[] fileList = directory.listFiles();
         for (File file : fileList) {
             if (file.getName().contains(index)) {
@@ -234,4 +192,82 @@ class App {
             }
         }
     }
+
+    public static int loadLastId() {
+        String filePath = BASE_DIRECTORY + "/lastId.txt";
+        try {
+            if (new File(filePath).exists()) {
+                return Integer.parseInt(Files.readString(Paths.get(filePath)).trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static List<WiseSaying> loadWiseSayings(int lastId) {
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+        File directory = new File(BASE_DIRECTORY);
+
+        if (directory.exists() && directory.isDirectory()) {
+            File[] files = directory.listFiles((dir, name) -> name.endsWith(".json") && !name.equals("lastId.txt") && !name.equals("data.json"));
+            if (files != null) {
+                for (File file : files) {
+                    try {
+                        WiseSaying wiseSaying = parseWiseSayingFile(file);
+                        if (wiseSaying != null) {
+                            wiseSayings.add(wiseSaying);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return wiseSayings;
+    }
+
+    public static WiseSaying parseWiseSayingFile(File file) throws IOException {
+        String[] lines = Files.readAllLines(file.toPath()).toArray(new String[0]);
+        try {
+            int id = Integer.parseInt(lines[1].split(":")[1].trim().replace(",", "").replace("\"", ""));
+            String quotes = lines[2].split(":")[1].trim().replace(",", "").replace("\"", "");
+            String name = lines[3].split(":")[1].trim().replace(",", "").replace("\"", "");
+            return new WiseSaying(id, quotes, name);
+        } catch (Exception e) {
+            System.err.println("Failed to parse file: " + file.getName());
+            return null;
+        }
+    }
+
+    public static void buildDataJsonFile() {
+        StringBuilder combinedJson = new StringBuilder();
+        File directory = new File(BASE_DIRECTORY);
+        combinedJson.append("[\n");
+        File[] files = directory.listFiles();
+        if (files != null && files.length > 0) {
+            for (int i = 0; i < files.length; i++) {
+                File file = files[i];
+                if (file.isFile() && !file.getName().equals("lastId.txt") && !file.getName().equals("data.json")) {
+                    try {
+                        String content = new String(Files.readAllBytes(Paths.get(file.getPath())));
+                        combinedJson.append(content.trim());
+                        if (i < files.length - 1) {
+                            combinedJson.append(",\n");
+                        }
+                        combinedJson.append("\n");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            combinedJson.append("]");
+            try {
+                writeToFile(BASE_DIRECTORY + "/data.json", combinedJson.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
